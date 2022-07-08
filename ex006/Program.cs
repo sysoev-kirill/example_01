fb48e00e0fe3b61b05833a38f276af09d53fb147
sysoev-kirill <sysoev.kirill@gmail.com>
2022-07-08 14:41:17 +0300

    ex06

---
+++ ex006/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Input user name: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "kirill")
+{
+   Console.WriteLine("Hey Kirill");
+}
+else
+{
+   Console.Write("Hello, ");
+   Console.WriteLine(username);
+}
